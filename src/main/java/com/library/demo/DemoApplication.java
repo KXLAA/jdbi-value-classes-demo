940286224924e3c448ee3e72804709986fa09d28
Kolade Afode <87288429+KXLAA@users.noreply.github.com>
2025-04-13 18:49:54 +0100

    fix: missing config

--- src/main/java/com/library/demo/DemoApplication.java
+++ src/main/java/com/library/demo/DemoApplication.java
@@ -1,8 +1,10 @@
 package com.library.demo;
 
+import org.jdbi.v3.spring.EnableJdbiRepositories;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableJdbiRepositories
 @SpringBootApplication
 public class DemoApplication {
 
